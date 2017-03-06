f41e34513685030a7390fed4ea67b995b3f5d862
Bubnov Slavik <bubnovslavik@gmail.com>
2017-03-06 23:33:05 +0100

    default cells implementations

--- TableViewAdapter/TableViewCells.swift
+++ TableViewAdapter/TableViewCells.swift
@@ -1,9 +1,45 @@
 //
 //  TableViewCells.swift
-//  Pods
+//  Collections
 //
 //  Created by Bubnov Slavik on 02/03/2017.
-//
+//  Copyright © 2017 Bubnov Slavik. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+public class UITableViewCellValue1: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+
+public class UITableViewCellValue2: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .value2, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+
+public class UITableViewCellSubtitle: UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
